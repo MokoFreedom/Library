d77a070228eb124cdaf904753c3b07a6a4410704
MokoFreedom <momoichigodayo@gmail.com>
2018-05-27 21:55:32 +0900

    delete template and fix dijkstra2

--- Python/Graph/dijkstra_2.py
+++ Python/Graph/dijkstra_2.py
@@ -1,24 +1,14 @@
 # coding: utf-8
 
-from collentions import defaultdict
+from collections import defaultdict
 from heapq import *
 
 
-class Graph:
+def dijkstra(edges, start, goal):
 
-    def __init__(self):
-        
-        g = defaultdict(list)
-
-    
-    def add_edge(from_node, to_node, cost, reverse_edge=False):
-
-        g[from_node].append((to_node, cost))
-        if reverse_edge:
-            g[to_node].append((from_node, cost))
-
-
-def dijkstra(g, start, goal):
+    g = defaultdict(list)
+    for l, r, c in edges:
+        g[l].append((c, r))
 
     q, seen = [(0, start, ())], set()
     
@@ -36,14 +26,3 @@ def dijkstra(g, start, goal):
                     heappush(q, (cost + next_cost, u, path))
 
     return float("inf")
-
-if __name__ == "__main__":
-	n = int(input())
-	g = Graph()
-	for i in range(n):
-		ls = [map(int, input().split())]
-		v = ls[0]
-		ls.pop(0)
-		for j in range(len(ls) / 2):
-			g.add_edge(v, ls[j * 2], ls[j * 2 + 1])
-	
